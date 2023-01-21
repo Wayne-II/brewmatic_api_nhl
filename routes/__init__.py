3f07f65a69c08760e4d56384bbbcd1c331a1161d
Wayne Marsh <wrinkledcheese@gmail.com>
2023-01-21 14:27:31 -0400

    adding the teams endpoint to the api

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,9 +1,11 @@
 from flask_restx import Api
 
 from .schedule import api as schedule_api
+from .teams import api as teams_api
 
 api = Api(
     title="NHL API"
 )
 
 api.add_namespace( schedule_api )
+api.add_namespace( teams_api )
