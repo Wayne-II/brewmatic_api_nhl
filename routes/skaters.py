c984d144a5fa556c374ce7c35770465e108e9bff
Wayne Marsh <wrinkledcheese@gmail.com>
2023-10-02 19:57:02 -0300

    adding notes about newer api to fetch mutiple players at once as well as cleanup some debug prints

--- routes/skaters.py
+++ routes/skaters.py
@@ -1,15 +1,22 @@
 from flask_restx import Namespace, Resource, fields
 from common import FetchJson
 
+#TODO: update NHL API for skaters to use new API as multiple people can be
+# fetched all at once up to 100 at a time.  One request per game scheduled 
+# should result in minimal server requests and quicker loading time.  Once data
+# is fetched, it should be stored locally and checked against the database 
+# before a request is sent to NHL - to avoid being banned 
+# the following url fetches the stats for 2 players with this part being the part that needs to be modified "playerId%20in%20%288478402,8478403%29"
+# which translates to SQL query where clause part "playerId in (8478402,8478403)"
+# https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=%5B%7B%22property%22:%22goals%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22points%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22gamesPlayed%22,%22direction%22:%22ASC%22%7D,%7B%22property%22:%22playerId%22,%22direction%22:%22ASC%22%7D%5D&start=0&limit=334&factCayenneExp=gamesPlayed%3E=1&cayenneExp=gameTypeId%3E=2%20and%20playerId%20in%20%288478402,8478403%29%20and%20seasonId%3C=20222023%20and%20seasonId%3E=20222023
 baseUrl = "https://statsapi.web.nhl.com/api/v1/people"
 #TODO: move season end year to request or configuration - maybe computed based
 # on month/year 
-seasonEndYear = 2023
+seasonEndYear = 2024
 
 # filter skater to only the required information
 def FilterSkater( skater ):
     skaterKeys = [ 'id', 'firstName', 'lastName', 'fullName', 'rosterStatus' ]
-    print( skater )
     return { key: skater[ key ] for key in skaterKeys }
 
 # filter stats so that only goals are returned
@@ -42,6 +49,7 @@ id_parser = api.parser()
 id_parser.add_argument('id', type=int, action='split')
 
 @api.route("/")
+@api.doc(params={"id": "Skater IDs - comma delimited list"})
 class Skaters( Resource ):
     def get( self ):
         args = id_parser.parse_args()
