7fc48e46b4e88eb1f8db707c03d7a7c2e5809500
Wayne Marsh <marsh.wayne@gmail.com>
2023-10-25 18:24:44 -0300

    load custom env

--- brewmatic_api_nhl.py
+++ brewmatic_api_nhl.py
@@ -1,3 +1,5 @@
+
+
 #TODO: error handling, design docs, tests, etc
 
 #TODO: store NHL API data on first request or schedule a daily fetch
@@ -11,6 +13,8 @@
 #POC: this is a proof of concept v2 using NHL public API instead of table
 #scaraping hockeyreference.com
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv('.env')
 from routes import api
 
 app = Flask(__name__)
