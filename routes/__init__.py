a2cb0a0c1791815b47565ad42228bd4430deaab4
Wayne Marsh <marsh.wayne@gmail.com>
2023-10-25 23:22:00 -0300

    adding suggest to api

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from .schedule import api as schedule_api
 from .teams import api as teams_api
 from .skaters import api as skaters_api
+from .suggest import api as suggest_api
 
 api = Api(
     title="NHL API"
@@ -11,3 +12,4 @@ api = Api(
 api.add_namespace( schedule_api )
 api.add_namespace( teams_api )
 api.add_namespace( skaters_api )
+api.add_namespace( suggest_api )
