516fee93f0b344ad761f94734af8e85338992281
Wayne Marsh <marsh.wayne@gmail.com>
2023-11-03 19:33:26 -0300

    copy pasta

--- schema/skaters.py
+++ schema/skaters.py
@@ -5,7 +5,7 @@ class Skater( Base ):
     __tablename__ = 'skaters'
     id = Column( Integer, primary_key=True )
     join_date = Column( Date, primary_key=True )
-    lase_name = Column( String ),
-    skater_full_name = Column( String ),
-    goals = Column( Integer ),
+    lase_name = Column( String )
+    skater_full_name = Column( String )
+    goals = Column( Integer )
     team_id = Column( Integer, ForeignKey( 'teams.id' ), nullable=False )
