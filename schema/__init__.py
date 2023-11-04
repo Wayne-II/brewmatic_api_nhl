5a710313df544642cafae71450eb8544ebccb84f
Wayne Marsh <marsh.wayne@gmail.com>
2023-11-04 19:28:07 -0300

    renamed schema to models

--- schema/__init__.py
+++
@@ -1,5 +0,0 @@
-from .schedule import Schedule
-from .skaters import Skater
-from .base import Base
-from .engine import engine
-from .teams import Team
--- schema/teams.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .base import Base
-
-class Team( Base ):
-    __tablename__ = 'teams'
-    id = Column( Integer, primary_key=True )
-    name = Column( String )
-    abbreviation = Column( String )
