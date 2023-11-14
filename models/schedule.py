03e562512a9fa826fb07f858c65176f2b5073bad
Wayne Marsh <marsh.wayne@gmail.com>
2023-11-13 22:04:05 -0400

    update schedule to reflect schema changes

--- models/schedule.py
+++ models/schedule.py
@@ -6,8 +6,8 @@ class Schedule( Base ):
     __tablename__ = 'schedule'
     __table_args__ = {'extend_existing':True}
     id = Column( Integer, primary_key=True, autoincrement=True )
-    home_id = Column( Integer, ForeignKey( 'teams.id' ) )
-    away_id = Column( Integer, ForeignKey( 'teams.id' ) )
+    home_id = Column( Integer, ForeignKey( 'teams.team_id' ) )
+    away_id = Column( Integer, ForeignKey( 'teams.team_id' ) )
     game_date = Column( Date )
 
 #TODO: new schedule API https://api-web.nhle.com/v1/scoreboard/
